c0e96c8b25b65ec9690855aec218cf2c15fa8e6b
delgarre <38860567+delgarre@users.noreply.github.com>
2021-04-06 08:30:50 -0500

    Update Orderhistory.java

--- src/main/java/com/learn/mycart/entities/Orderhistory.java
+++ src/main/java/com/learn/mycart/entities/Orderhistory.java
@@ -1,39 +1,204 @@
 
 package com.learn.mycart.entities;
 
-import java.util.Set;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
+import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
 
 @Entity
-public class Orderhistory {
+public class OrderHistory implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int orderId;
+    private int id;
     
+    private String aName;
     
-    private String userName;
+    private double aPrice;
     
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name="company_order", joinColumns={@JoinColumn(referencedColumnName="orderId")}
-                                , inverseJoinColumns={@JoinColumn(referencedColumnName="companyId")})
+    private String cName;
     
-    private Set<Company> companies;
+    private int quantity;
     
-    @Column(name="company_name")
-    private String companyName;
+    private String date;
+    
+    private String locations;
+    
+    private String itemNumber;
+    
+   private String photo;
+   
+   private String manufacturer;
+   
+    private String manufacturerNum;
+    
+    private String alternateItem;
+    
+    private String pDesc;
+    
+    private String cTitle;
+    
+    private String vTitle;
+    
+    private String unitOfMeasure;
 
-    public Orderhistory() {
+    public OrderHistory() {
     }
+
+    public OrderHistory(int id, String aName, double aPrice, String cName, int quantity, 
+            String date, String locations, String itemNumber, String photo, String cTitle, String vTitle
+            , String unitOfMeasure, String manufacturer, String manufacturerNum, String alternateItem, String pDesc) {
+        this.id = id;
+        this.aName = aName;
+        this.aPrice = aPrice;
+        this.cName = cName;
+        this.quantity = quantity;
+        this.date = date;
+        this.locations = locations;
+        this.itemNumber = itemNumber;
+        this.photo = photo;
+        this.cTitle = cTitle;
+        this.vTitle = vTitle;
+        this.unitOfMeasure = unitOfMeasure;
+        this.manufacturer = manufacturer;
+        this.manufacturerNum = manufacturerNum;
+        this.alternateItem = alternateItem;
+        this.pDesc = pDesc;
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getaName() {
+        return aName;
+    }
+
+    public void setaName(String aName) {
+        this.aName = aName;
+    }
+
+    public double getaPrice() {
+        return aPrice;
+    }
+
+    public void setaPrice(double aPrice) {
+        this.aPrice = aPrice;
+    }
+
+    public String getcName() {
+        return cName;
+    }
+
+    public void setcName(String cName) {
+        this.cName = cName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocations() {
+        return locations;
+    }
+
+    public void setLocations(String locations) {
+        this.locations = locations;
+    }
+
+    public String getItemNumber() {
+        return itemNumber;
+    }
+
+    public void setItemNumber(String itemNumber) {
+        this.itemNumber = itemNumber;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturerNum() {
+        return manufacturerNum;
+    }
+
+    public void setManufacturerNum(String manufacturerNum) {
+        this.manufacturerNum = manufacturerNum;
+    }
+
+    public String getAlternateItem() {
+        return alternateItem;
+    }
+
+    public void setAlternateItem(String alternateItem) {
+        this.alternateItem = alternateItem;
+    }
+
+    public String getpDesc() {
+        return pDesc;
+    }
+
+    public void setpDesc(String pDesc) {
+        this.pDesc = pDesc;
+    }
+
+    public String getcTitle() {
+        return cTitle;
+    }
+
+    public void setcTitle(String cTitle) {
+        this.cTitle = cTitle;
+    }
+
+    public String getvTitle() {
+        return vTitle;
+    }
+
+    public void setvTitle(String vTitle) {
+        this.vTitle = vTitle;
+    }
+
+    public String getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(String unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
+    
     
     
 }
